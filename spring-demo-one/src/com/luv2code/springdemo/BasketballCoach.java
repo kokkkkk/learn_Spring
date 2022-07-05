3e4f636a2295bdd44b019f6ebfa536df09f31819
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2022-07-05 16:22:28 +0800

    Practice Activity #1

---
+++ spring-demo-one/src/com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Shoot 30 minutes";
+    }
+}
