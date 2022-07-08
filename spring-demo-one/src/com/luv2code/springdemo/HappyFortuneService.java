276d53f88773a89f2363aa0a7c19f782ac586712
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2022-07-08 15:22:36 +0800

    Practice Activity #2

--- spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
+++ spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,8 +1,15 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService{
+
+    private String[] fortuneOption = {"Happy","Sad","Normal"};
+    private Random random = new Random();
+
     @Override
-    public String getFortune() {
-        return "Today is your lucky day!";
+    public String getFortune(){
+        int num = random.nextInt(3);
+        return fortuneOption[num];
     }
 }
