31fdb60d2d88ecc6f21933504a723d17e4197bed
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2022-07-08 14:37:01 +0800

    Complete

--- spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -4,7 +4,21 @@ public class CricketCoach implements Coach{
 
     private  FortuneService fortuneService;
 
+    //add new fields for emailAddress and team
+    private  String emailAddress;
+    private  String Team;
+
     //our setter method
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        Team = team;
+    }
+
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
@@ -18,6 +32,14 @@ public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return Team;
     }
 
     @Override
