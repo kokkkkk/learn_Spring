f7a40f259f130a732c74eeeda349cabdefb4e522
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2022-07-13 16:14:28 +0800

    Constructor Injection:Part 2

--- spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -11,6 +11,8 @@ public class AnnotationDemoApp {
         //get the bean from spring container
         Coach theCoach = context.getBean("tennisCoach",Coach.class);
 
+        //call method to get the daily fortune
+        System.out.println(theCoach.getDailyFortune());
         //call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
 
