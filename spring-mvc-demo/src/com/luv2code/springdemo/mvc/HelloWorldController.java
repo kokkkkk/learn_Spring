7b6c8e1aab654be0ffa69aeee943424e90036294
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2022-09-02 20:39:20 +0800

    Adding Data to the Spring Model - part 2

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -35,7 +35,7 @@ public class HelloWorldController {
 		theName = theName.toUpperCase();
 		
 		//create the message
-		String result = "Yo!"+theName;
+		String result = "Yo! "+theName;
 		
 		//add message to the model
 		model.addAttribute("message",result);
