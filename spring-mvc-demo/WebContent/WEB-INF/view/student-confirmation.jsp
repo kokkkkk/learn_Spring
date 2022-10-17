b3e88224aa017c59c51cd9a2afc007f666c8c659
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2022-10-17 20:21:25 +0800

    Radio Button

--- spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
@@ -12,6 +12,10 @@ The student is confirmed: ${student.firstName} ${student.lastName}
 <br><br>
 
 Country: ${student.country}
+
+<br><br>
+
+Favorite Language: ${student.favoriteLanguage }
 </body>
 
 </html>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -11,6 +11,8 @@ public class Student {
 	
 	private LinkedHashMap<String,String> countryOptions;
 	
+	private String favoriteLanguage;
+	
 	public Student() {
 		//populate country options: used ISO country code
 		countryOptions = new LinkedHashMap<>();
@@ -48,6 +50,14 @@ public class Student {
 
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
 	}
 	
 	
