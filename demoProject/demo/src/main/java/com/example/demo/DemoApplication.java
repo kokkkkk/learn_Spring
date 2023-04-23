182a9cf8fad291d112e017aaadadb21fb4424229
kokkkkk <ko.kiki12355@gmail.com>
2023-04-22 21:29:09 -0400

    API Layer

--- demoProject/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demoProject/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -20,16 +12,4 @@ public class DemoApplication {
 
 	}
 
-	@GetMapping("/")
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Mariam",
-						"mariam.jamel@gmail.com",
-						LocalDate.of(2000, Month.JANUARY,5),
-						21
-				)
-		);
-	}
 }
